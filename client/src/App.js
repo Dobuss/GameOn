0cd6eab81b73f9e813855a0ed8ad306402b35583
Dobuss <dobromira.ivanova911@gmail.com>
2023-03-20 18:41:22 +0200

    fixing app structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Team } from "./components/Team";
+import { Team } from "./components/teamInfo/Team";
 import { AddNewGame } from "./components/gamesCatalogue/AddNewGame";
 import { Catalogue } from "./components/gamesCatalogue/Catalogue";
 import { EditGame } from "./components/gamesCatalogue/EditGame";
 import { GameDetails } from "./components/gamesCatalogue/GameDetails";
 import { Header } from "./components/gamesCatalogue/Header";
 import { AboutUs } from "./components/homePage/AboutUs";
-import { Footer } from "./components/homePage/Footer";
+import { Footer } from "./components/globalComponents/Footer";
 import { ImageInfo } from "./components/homePage/ImageInfo";
-import { Navigation } from "./components/homePage/Navigation";
+import { Navigation } from "./components/globalComponents/Navigation";
 import { Topbar } from "./components/homePage/Topbar";
 import { Login } from "./components/user/Login";
 import { Register } from "./components/user/Register";
-import { NotFound } from "./components/NotFound";
+import { NotFound } from "./components/pageNotFound/NotFound";
 
 function App() {
 
--- client/src/components/gamesCatalogue/modals/DeletionMessage.js
+++ client/src/components/gamesCatalogue/modals/DeletionMessage.js
@@ -6,7 +6,7 @@ export const DeletionMessage = ({
     
     const message = (
         <div>
-            <p className='ms-3 fw-bold mb-2'>Are you sure you want to delete this game?</p>
+            <p className='ms-3 fw-bold mb-2' style={{marginTop: '10px'}}>Are you sure you want to delete this game?</p>
             <button className="text-uppercase fw-bold btn" onClick={onConfirm}>Delete</button>
             <button className="text-uppercase fw-bold btn" onClick={onClose}>Cancel</button>
         </div>
