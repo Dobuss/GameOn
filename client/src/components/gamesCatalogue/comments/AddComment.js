71e4e64beb791cdcf7cf862987bcacea48101bfd
Dobuss <dobromira.ivanova911@gmail.com>
2023-04-05 18:01:38 +0300

    fixing comment after git error

--- client/src/components/gamesCatalogue/comments/AddComment.js
+++ client/src/components/gamesCatalogue/comments/AddComment.js
@@ -24,30 +24,28 @@ export const AddComment = () => {
     }
     
     return (
-        <div className="container-fluid py-6 px-5">
-        <div className="row gx-5">
-            <div className="col-lg-4 mb-5 mb-lg-0">
-                <div className="mb-4">
-                    <h1 className="display-5 text-uppercase mb-4">Add new <span className="text-primary">Comment</span> to the Game</h1>
-                </div>
-                <p className="mb-5" style={{fontSize: 30}}>We are happy to hear from you. <i className="far fa-smile-beam"></i></p>
+        <div className={styles.container}>
+        <div className={styles.contentWrapper}>
+            <div className={styles.col1}>
+                    <h1 className={styles.heading}>Add new <span className={styles.highlight}>Comment</span> to the Game</h1>
+                <p className={styles.paragraph}>We are happy to hear from you. <i className="far fa-smile-beam"></i></p>
             </div>
-            <div className="col-lg-8">
-        <div className="bg-light text-center p-5">
+            <div className={styles.col2}>
+        <div className={styles.col2Wrapper}>
                     <form onSubmit={onCommentSubmit}>
-                        <div className="row g-3">
-                            <div className="col-12">
-                                <label className="newGameLbl" htmlFor="author">Author</label>
-                                <input type="text" id="author" className="form-control border-0" style={{height: 55}} value={author} onChange={(e) => setAuthor(e.target.value)}/>
+                        <div className={styles.form}>
+                            <div className={styles.maxWidthWrapper}>
+                                <label className={styles.newGameLbl} htmlFor="author">Author</label>
+                                <input type="text" id="author" className={styles.input} value={author} onChange={(e) => setAuthor(e.target.value)}/>
                             </div>
-                            <div className="col-12">
-                            <label className="newGameLbl" htmlFor="description">Comment</label>
-                                <textarea className="form-control border-0" id="description" rows="5" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
+                            <div className={styles.maxWidthWrapper}>
+                            <label className={styles.newGameLbl} htmlFor="description">Comment</label>
+                                <textarea className={`${styles.input} ${styles.inputComment}`} id="description" rows="5" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
                             </div>
-                            <div className="col-12">
-                            <button className="btn btn-primary w-100 py-3" type="submit">Add Comment</button>
+                            <div className={styles.maxWidthWrapper}>
+                            <button className={styles.btn} type="submit">Add Comment</button>
                             <p></p>
-                            <button className="btn btn-primary w-100 py-3" type="button" onClick={onCancel}>Cancel</button>
+                            <button className={styles.btn} type="button" onClick={onCancel}>Cancel</button>
                             </div>
                         </div>
                     </form>
